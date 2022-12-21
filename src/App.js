420ba2970eadf573e7cfe4717eb44b8a5396c007
Malavika1113 <74175454+Malavika1113@users.noreply.github.com>
2022-12-21 20:28:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
-import React from 'react';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({data: []});
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState('');
+
+  const handleClick = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(https://api.github.com/repos/Malavika1113/react-workflow-gh-actions/actions/workflows/Reuseable-A.yml, {
+        method: 'GET',
+        headers: {
+		  auth: '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'
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+
+      console.log('result is: ', JSON.stringify(result, null, 4));
+
+      setData(result);
+    } catch (err) {
+      setErr(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Data Ext Form
-        </p>
-        <form>
-         <div className="input-container">
-           <label>Release Version </label>
-           <input type="text" name="uname" required />
-         </div>
-         <div className="input-container">
-           <label>Model Type </label>
-           <input type="text" name="pass" required />
-         </div>
-         <div className="button-container">
-           <input type="submit" />
-         </div>
-        </form>
-      </header>
+    <div>
+      {err && <h2>{err}</h2>}
+
+      <button onClick={handleClick}>Fetch data</button>
+
+      {isLoading && <h2>Loading...</h2>}
+
+      {data.data.map(person => {
+        return (
+          <div key={person.id}>
+            <h2>{person.name}</h2>
+            <h2>{person.path}</h2>
+            <br />
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
