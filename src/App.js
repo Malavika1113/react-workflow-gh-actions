60e03bf0d414f65166e4567e8bc28214f2cab7ec
Malavika1113 <74175454+Malavika1113@users.noreply.github.com>
2022-12-21 12:01:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,20 +8,20 @@ function App() {
         <p>
           Data Form
         </p>
+        <form>
+         <div className="input-container">
+           <label>Username </label>
+           <input type="text" name="uname" required />
+         </div>
+         <div className="input-container">
+           <label>Password </label>
+           <input type="password" name="pass" required />
+         </div>
+         <div className="button-container">
+           <input type="submit" />
+         </div>
+        </form>
       </header>
-      <form>
-       <div className="input-container">
-         <label>Username </label>
-         <input type="text" name="uname" required />
-       </div>
-       <div className="input-container">
-         <label>Password </label>
-         <input type="password" name="pass" required />
-       </div>
-       <div className="button-container">
-         <input type="submit" />
-       </div>
-     </form>
     </div>
   );
 }
