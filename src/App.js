4d2c5dfd38ca4e7152afcc5fda0484a1697874a0
Malavika1113 <74175454+Malavika1113@users.noreply.github.com>
2022-12-22 12:27:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 const App = () => {
   const [data, setData] = useState({data: []});
-  const [isLoading, setIsLoading] = useState(false);
-  const [err, setErr] = useState('');
+  const [setIsLoading] = useState(false);
+  const [setErr] = useState('');
   let userasd = {
   	ref: 'master'
 	};
