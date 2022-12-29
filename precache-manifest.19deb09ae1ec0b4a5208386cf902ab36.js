cea1c4068576bb2f64e7e35d1c2e50132f1aac0d
Hosted Agent <github-actionsPages@users.noreply.github.com>
2022-12-29 05:57:11 +0000

    Updates

--- precache-manifest.19deb09ae1ec0b4a5208386cf902ab36.js
+++ precache-manifest.19deb09ae1ec0b4a5208386cf902ab36.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6d91e8a4d795f14dcb0351b288127edc",
+    "revision": "27a26f515c8b26ae67ba8fc6538a2a6c",
     "url": "/react-workflow-gh-actions/index.html"
   },
   {
-    "revision": "c676db2cb31ec2476738",
+    "revision": "6696558a9ba8301b6008",
     "url": "/react-workflow-gh-actions/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-workflow-gh-actions/static/js/2.3cb964e6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c676db2cb31ec2476738",
-    "url": "/react-workflow-gh-actions/static/js/main.fcc2a748.chunk.js"
+    "revision": "6696558a9ba8301b6008",
+    "url": "/react-workflow-gh-actions/static/js/main.d02f8f53.chunk.js"
   },
   {
     "revision": "448d559345594dc72292",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-workflow-gh-actions/precache-manifest.672e3bc99bfffa6885311e43822827c4.js"
+  "/react-workflow-gh-actions/precache-manifest.19deb09ae1ec0b4a5208386cf902ab36.js"
 );
 
 self.addEventListener('message', (event) => {
